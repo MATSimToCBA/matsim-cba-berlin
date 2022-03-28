8261bc5edb2f0598415891cc3545cd03795bbf00
tkchouaki <tarek.chouaki@irt-systemx.fr>
2022-03-28 17:41:15 +0200

    Added DRT Speed Up feature and some refractoring

--- src/main/java/org/matsim/run/drt/RunDrtOpenBerlinScenarioWithCba.java
+++ src/main/java/org/matsim/run/drt/RunDrtOpenBerlinScenarioWithCba.java
@@ -2,33 +2,56 @@ package org.matsim.run.drt;
 
 import org.apache.log4j.Logger;
 import org.matsim.api.core.v01.Scenario;
+import org.matsim.api.core.v01.population.Person;
 import org.matsim.contrib.cba.CbaConfigGroup;
 import org.matsim.contrib.cba.CbaModule;
+import org.matsim.contrib.drt.run.DrtConfigGroup;
+import org.matsim.contrib.drt.run.MultiModeDrtConfigGroup;
+import org.matsim.contrib.drt.speedup.DrtSpeedUpParams;
 import org.matsim.core.config.Config;
 import org.matsim.core.controler.Controler;
+import org.matsim.run.utils.ResumeSimulationFromLastPlans;
+import org.matsim.core.config.CommandLine;
+
 
 public class RunDrtOpenBerlinScenarioWithCba {
-    private static final Logger log = Logger.getLogger(RunDrtOpenBerlinScenarioWithCba.class);
 
-    public static void main(String[] args) {
-        for (String arg : args) {
-            log.info( arg );
-        }
-        if (args.length==0 ) {
-            args = new String[] {"scenarios/berlin-v5.5-1pct/input/drt/berlin-drt-v5.5-cba-pt-ag-pv-drt-fr1-1pct.config.xml"}  ;
-        }
-        Config config = prepareConfig(args);
-        Scenario scenario = prepareScenario(config);
+    public static void main(String[] args) throws CommandLine.ConfigurationException {
+        CommandLine cmd = new CommandLine.Builder(args)
+                .requireOptions("config-path")
+                .allowOptions("drt-speed-up", "resume-simulation")
+                .build();
+        String configPath = cmd.getOptionStrict("config-path");
+        boolean resumeSimulation = cmd.hasOption("resume-simulation") && Boolean.parseBoolean(cmd.getOptionStrict("resume-simulation"));
+        boolean drtSpeedUp = cmd.hasOption("drt-speed-up") && Boolean.parseBoolean(cmd.getOptionStrict("drt-speed-up"));
+        Config config = prepareConfig(new String[]{configPath}, drtSpeedUp, resumeSimulation);
+        cmd.applyConfiguration(config);
+        Scenario scenario = prepareScenario(config, drtSpeedUp);
         Controler controler = prepareControler(scenario);
         controler.run();
     }
 
-    public static Config prepareConfig(String[] args) {
-        return RunDrtOpenBerlinScenario.prepareConfig(args, new CbaConfigGroup());
+    public static Config prepareConfig(String[] args, boolean drtSpeedUp, boolean resumeSimulation) {
+        Config config = RunDrtOpenBerlinScenario.prepareConfig(args, new CbaConfigGroup());
+        if(resumeSimulation) {
+            ResumeSimulationFromLastPlans.resumeSimulationFromLastPlans(config);
+        }
+        if(drtSpeedUp) {
+            for (DrtConfigGroup drtCfg : MultiModeDrtConfigGroup.get(config).getModalElements()) {
+                if (drtCfg.getDrtSpeedUpParams().isEmpty()) {
+                    drtCfg.addParameterSet(new DrtSpeedUpParams());
+                }
+            }
+        }
+        return config;
     }
 
-    public static Scenario prepareScenario(Config config) {
-        return RunDrtOpenBerlinScenario.prepareScenario(config);
+    public static Scenario prepareScenario(Config config, boolean drtSpeedUp) {
+        Scenario scenario = RunDrtOpenBerlinScenario.prepareScenario(config);
+        for (Person person : scenario.getPopulation().getPersons().values()) {
+            person.getPlans().removeIf((plan) -> plan != person.getSelectedPlan());
+        }
+        return scenario;
     }
 
     public static Controler prepareControler(Scenario scenario) {
